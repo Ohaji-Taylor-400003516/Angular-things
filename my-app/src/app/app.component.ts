446863b27ffc53f2f83d7d490686110e66d956a2
OHaji Taylor <ohaji.taylor@mycavehill.uwi.edu>
2023-01-23 00:25:57 -0400

    init

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  favAmi: string="cat";
+
+  constructor() {
+   
+  }
+}
